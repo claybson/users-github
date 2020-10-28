03a2aee86fb73e49d492cc5731ae2a1c7f428e89
Claybson <claybson@cartello.com.br>
2020-10-27 22:14:36 -0300

    Incluindo detalhes do repositório e finalizando projeto

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash, FaSearch } from 'react-icons/fa';
-import {Container, Content, Form, Perfil, SubmitButton, List, Sidebar} from './styles';
-
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash, FaSearch, FaMapMarkerAlt, FaBuilding, FaUserFriends, FaDatabase } from 'react-icons/fa';
+import {Container, Content, Heading, Form, Perfil, SubmitButton, List, Sidebar} from './styles';
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 export default function Main(){
@@ -9,6 +9,7 @@ export default function Main(){
   const [newUser, setNewUser] = useState('');
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [load, setLoad] = useState(false);
   const [alert, setAlert] = useState(null);
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
@@ -40,11 +41,22 @@ export default function Main(){
 
   useEffect(()=>{
     localStorage.setItem('repos', JSON.stringify(repositorios));
+    repositorios.sort(function compare(a, b) {
+      var dateA = new Date(a.date);
+      var dateB = new Date(b.date);
+      return dateA - dateB;
+    });
   }, [repositorios]);
 
   useEffect(()=>{
     localStorage.setItem('list', JSON.stringify(list));
   }, [list]);
+
+  useEffect(()=>{
+    setLoad(localStorage.getItem('load'));
+  }, [load]);
+
+  
 
   const handleSubmit = useCallback((e)=>{
     e.preventDefault();
@@ -52,34 +64,41 @@ export default function Main(){
     async function submit(){
       setLoading(true);
       setAlert(null);
+      
       try{
 
         if(newUser === ''){
           throw new Error('Você precisa indicar um usuário!');
         }
 
-        const response = await api.get(`${newUser}`);
-        const lista = await api.get(`${newUser}/repos`);
+        localStorage.setItem('load', true);
+        setLoad(localStorage.getItem('load'));
+
+        const response = await api.get(`users/${newUser}`);
+        const lista = await api.get(`users/${newUser}/repos`);
         setUsers(response.data);
-        setList([...list, response.data]);
-        
-
-        const hasUser = list.find(user => user.name === newUser);
-
-        if(hasUser){
-          throw new Error('Usuário Duplicado');
-        }
 
         let reposi = [];
         lista.data.map((repo) => {
           let data = {
             name: repo.name,
-            date: repo.updated_at
+            date: repo.updated_at,
+            login: repo.owner.login
           }
           reposi = [...reposi, data];
         })
 
         setRepositorios(reposi);
+
+        const hasUser = list.find(user => user.login === newUser);
+
+        if(hasUser){
+          throw new Error('Usuário Duplicado');
+        }else{
+          setList([...list, response.data]);
+        }
+
+        
 
         setNewUser('');
       }catch(error){
@@ -100,62 +119,76 @@ export default function Main(){
     setAlert(null);
   }
 
-
   return(
     <Container>
+
+        <Heading>
+          <h1>
+            <FaGithub size={25}/>
+            Buscar Usuários
+          </h1>
+
+          <Form onSubmit={handleSubmit} error={alert}>
+          
+            <input 
+            type="text" 
+            placeholder="Buscar"
+            value={newUser}
+            onChange={handleinputChange}
+            />
+
+            <SubmitButton loading={loading ? 1 : 0}>
+              {loading ? (
+                <FaSpinner color="#FFF" size={14}/>
+              ) : (
+                <FaSearch color="#FFF" size={14}/>
+              )}
+            </SubmitButton>
+
+          </Form>
+        </Heading>
+        { load  ? (
       <Content>
-        <h1>
-          <FaGithub size={25}/>
-          Buscar Usuários
-        </h1>
-
-        <Form onSubmit={handleSubmit} error={alert}>
-          <input 
-          type="text" 
-          placeholder="Buscar"
-          value={newUser}
-          onChange={handleinputChange}
-          />
-
-          <SubmitButton loading={loading ? 1 : 0}>
-            {loading ? (
-              <FaSpinner color="#FFF" size={14}/>
-            ) : (
-              <FaSearch color="#FFF" size={14}/>
-            )}
-          </SubmitButton>
-
-        </Form>
-
+      
         <Perfil>
               <figure>
                 <img src={users.avatar_url}/>
               </figure>
               <div className="info">
-                <a href={users.html_url}  target="_blank" class="info__name">{users.name}</a>
-                <h3>{users.bio}</h3>
+                <a href={users.html_url}  target="_blank" className="info__name">{users.name}</a>
+                <h3 className="info__bio">{users.bio}</h3>
                 <div>
-                  <span>{users.followers} <a href={users.html_url+'?tab=followers'} target="_blank">Followers</a></span> - <span>{users.following} <a href={users.html_url+'?tab=following'} target="_blank">Following</a></span>
+                  <span><FaUserFriends/> {users.followers} <a href={users.html_url+'?tab=followers'} target="_blank">Followers</a></span> - <span>{users.following} <a href={users.html_url+'?tab=following'} target="_blank">Following</a></span>
                 </div>
-                <h4>{users.company}</h4>
-                <h4>{users.location}</h4>
+                <h4 className="info__company"><FaBuilding/> {users.company}</h4>
+                <h4 className="info__location"><FaMapMarkerAlt/> {users.location}</h4>
               </div>
         </Perfil>
-
+  
         <List>
+          <h2><FaDatabase/> Repositórios</h2>
           {repositorios.map(repo => (
             <li key={repo.name}>
               <span>
               {repo.name}
               </span>
+              <Link to={`/repositorio/${encodeURIComponent(repo.login+'/'+repo.name)}`}>
+               <FaBars size={20}/>
+             </Link>
             </li>
           ))} 
         </List>
+        
       </Content>
-
+         ) : ( 
+          <></>
+        )}
+      {load  ? (
       <Sidebar>
+        <h2>Últimas Buscas</h2>
       <ul>
-      {list.map(user => (
+
+      {list.reverse().slice(0, 5).map(user => (
           <li key={user.name}>
             <span>
             {user.name}
@@ -164,6 +197,10 @@ export default function Main(){
         ))} 
       </ul>
       </Sidebar>
+
+      ) : ( 
+        <></>
+      )}
 
     </Container>
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,11 @@
 import styled, {keyframes, css} from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  max-width: 1200px;
-  background: #FFF;
+  display: block;
+  max-width: 1100px;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0,0,0, 0.2);
-  padding: 30px;
+  padding: 0 15px;
   margin: 80px auto;
 
   h1{
@@ -22,18 +21,24 @@ export const Container = styled.div`
   }
 
 
+
 `;
 
-export const Content = styled.div`
-  
-  @media(min-width: 992px){
-    width: 70%;
-    float: left;
+
+export const Heading = styled.div`
+  background: #fff;
+  padding: 30px;
+  margin: 0 0 30px;
+  border-radius: 5px;
+
+  h1 {
+    margin: 0 0 20px;
   }
+
+  
 `
 
 export const Form = styled.form`
-  margin-top: 30px;
   display:flex;
   flex-direction: row;
 
@@ -47,12 +52,108 @@ export const Form = styled.form`
 
 `;
 
+
+export const Content = styled.div`
+  background: #fff;
+  border-radius: 5px;
+  max-width: 600px;
+  margin: 0 auto;
+
+  @media(min-width: 992px){
+    width: 100%;
+    max-width: 700px;
+    float: left;
+  }
+  
+  
+`
+
 export const Perfil = styled.div`
+  max-width: 300px;
+  padding: 30px;
+  margin: 0 auto;
+
+  figure {
+    img {
+      max-width: 200px;
+      display: table;
+      margin: 0 auto;
+      border-radius: 99px;
+    }
+  }
+
+  .info {
+    text-align: center;
+    padding: 20px 0;
+
+    &__name {
+      font-size: 20px;
+      text-decoration: none;
+      color: #222;
+      font-weight: bold;
+      margin: 0 0 10px;
+      display: block;
+    }
+
+    &__bio {
+      font-size: 16px;
+      font-style: italic;
+      font-weight: unset;
+      margin: 0 0 20px;
+    }
+
+    div {
+      text-align: left;
+      margin: 0 0 5px;
+
+      span, a {
+        font-size: 12px;
+        font-weight: bold;
+        color: rgba(34, 34, 34, 0.6);
+        text-decoration: none;
+      }
+    }
+
+    &__company, &__location {
+      font-size: 12px;
+      color: rgba(34, 34, 34, 0.6);
+      text-align: left;
+      // font-weight: unset;
+      margin: 0 0 5px;
+    }
+
+
+  }
+
 
 `
 
 export const Sidebar = styled.div`
+  background: #fff;
+  border-radius: 5px;
+  margin: 30px auto;
+  padding: 30px;
+  max-width: 340px;
 
+  h2 {
+    font-size: 20px;
+    margin: 0 0 10px;
+  }
+
+  ul {
+    list-style: none;
+
+    li {
+      font-size: 14px;
+      padding: 5px 0;
+    }
+  }
+
+  @media (min-width: 992px){
+    float: right;
+    width: 100%;
+    margin: 0;
+  }
 `
 
 //Criando animcação do botao
@@ -99,7 +200,19 @@ export const SubmitButton = styled.button.attrs(props => ({
 
 export const List = styled.ul`
   list-style:none;
-  margin-top: 20px;
+  padding: 0 30px 30px;
+  max-width: 500px; 
+  margin: 0 auto;
+
+  h2 {
+    font-size: 20px;
+    margin: 0 0 20px;
+    display: flex;
+
+    svg {
+      margin-right: 5px;
+    }
+  }
 
   li{
     padding: 15px 0;
@@ -121,5 +234,7 @@ export const List = styled.ul`
   }
 
 `;
+
+
 
 
